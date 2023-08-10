6b31be13b7ff6468201ff77d3a9fe5c21e77f7df
Ridhwaan Ali <129802262+ridhwaanali@users.noreply.github.com>
2023-08-10 18:33:39 -0400

    app.py

--- Site:Code/app.py
+++ Site:Code/app.py
@@ -1,27 +1,18 @@
-#importing libraries 
-import json
-from flask import flask 
-from map import map 
-from scatter import scatter
-from line import line
+from flask import Flask, render_template 
+from lines import lines
+from scatters import scatters
+from bars import bars
 
-#importing and reading json
-f=  open('final.json')
-data= json.load(f)
-for i in data['emp_details']:
-    print (i)
-    f.close()
-
-#creating flask app
-app= Flask(__name__)
+app= Flask(__name__, template_folder= 'Templates')
 @app.route("/")
-
 def home():
     return render_template("Home.html")
-app.register_blueprint(map, url_prefix="/map")
-app.register_blueprint(map, url_prefix="/scatter")
-app.register_blueprint(map, url_prefix="/line")
 
-if __name__ --'__main__':
+
+app.register_blueprint(lines, url_prefix="/linesÍ")
+app.register_blueprint(scatters, url_prefix="/scatters")
+app.register_blueprint(bars, url_prefix="/Bars")
+
+if __name__ == '__main__':
     app.run(debug=True, port=8000)
-    
+
